d1f2de67495b5d7c8715ffd878ad6069f9afa545
Nandini <nandinimanjunath2008@gmail.com>
2026-07-31 11:56:00 +0530

    TOH

---
+++ n2.py
@@ -0,0 +1,9 @@
+def toh(n,s,d,a):
+    if n==1:
+        print("move disk 1 from source",s," to destination",d)
+        return
+    toh(n-1,s,a,d)
+    print("move disk",n,"from source",s,"to destination",d)
+    toh(n-1,a,d,s)
+n=int(input("enter the no of disk : "))
+toh(n,'s','a','d')
